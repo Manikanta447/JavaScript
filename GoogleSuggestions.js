558ed6c2c9f05311405607db63738d6e81d3e4c5
Jyothula Manikanta <141295144+Manikanta447@users.noreply.github.com>
2024-05-24 23:21:12 +0530

    Create GoogleSuggestions.js

---
+++ GoogleSuggestions.js
@@ -0,0 +1,122 @@
+// src/App.js
+import GoogleSuggestions from './components/GoogleSuggestions'
+
+import './App.css'
+
+const suggestionsList = [
+  {id: 1, suggestion: 'Price of Ethereum'},
+  {id: 2, suggestion: 'Oculus Quest 2 specs'},
+  {id: 3, suggestion: 'Tesla Share Price'},
+  {id: 4, suggestion: 'Price of Ethereum today'},
+  {id: 5, suggestion: 'Latest trends in AI'},
+  {id: 6, suggestion: 'Latest trends in ML'},
+]
+
+const App = () => {
+  ;<GoogleSuggestions suggestionsList={suggestionsList} />
+}
+
+export default App
+
+// src/Components/GoogleSuggestions
+
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  updateSearchInput = value => {
+    this.setState({searchInput: value})
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  render() {
+    console.log('rendering')
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+    console.log(searchResults)
+
+    return (
+      <div className="app-container">
+        <div className="google-suggestions-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-logo"
+          />
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
+              <img
+                alt="search icon"
+                className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              />
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeSearchInput}
+                value={searchInput}
+                id="input-element"
+              />
+            </div>
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
+                <SuggestionItem
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
+
+// src/components/SuggestionItem
+
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
+    </li>
+  )
+}
